d7edaaa2202e9a1e59c5a17ad2949ff720e79b51
Adam Brown <acourtneybrown@gmail.com>
2019-07-05 10:41:20 -0700

    Split scripts into setup, bootstrap & update

--- scripts/golang-setup.sh
+++ scripts/golang-setup.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-# Only run xcode-select when found (on MacOS)
-[ $(which xcode-select) ] && xcode-select --install
-
+brew install go
 go get -u github.com/go-delve/delve/cmd/dlv
 go get -u rsc.io/goversion
 go get -u github.com/codejanovic/gordon
--- scripts/ruby-setup.sh
+++ scripts/ruby-setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+brew install rbenv
 PATH=/usr/bin:$PATH rbenv install 2.1.9
