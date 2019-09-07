63f3cd2c5ddbfe7d237a4cfd257b6db2cd7330a8
Adam Brown <acourtneybrown@gmail.com>
2019-09-07 15:01:03 -0700

    Update ssh configs

--- script/golang-setup.sh
+++ script/golang-setup.sh
@@ -4,3 +4,5 @@ brew install go
 go get -u github.com/go-delve/delve/cmd/dlv
 go get -u rsc.io/goversion
 go get -u github.com/codejanovic/gordon
+go get -u github.com/github/mu/mu
+go get -u golang.org/x/tools/cmd/godoc
