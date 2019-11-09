bf9a6f66990c90aa871f0c39a8ec850acdafcd19
Adam Brown <acourtneybrown@gmail.com>
2019-11-08 23:01:39 -0700

    Do not install github/mu

--- script/golang-setup.sh
+++ script/golang-setup.sh
@@ -4,7 +4,6 @@ brew install go
 go get -u github.com/go-delve/delve/cmd/dlv
 go get -u rsc.io/goversion
 go get -u github.com/codejanovic/gordon
-go get -u github.com/github/mu/mu
 go get -u golang.org/x/tools/cmd/godoc
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/lint/golint
