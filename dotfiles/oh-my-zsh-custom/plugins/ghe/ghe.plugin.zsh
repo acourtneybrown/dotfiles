e943fbde719ade29bf30d9dd5ce58daee0a5d902
Adam Brown <acourtneybrown@gmail.com>
2020-05-21 10:00:08 -0700

    Ensure that Docker credentials are present before ghe build

--- dotfiles/oh-my-zsh-custom/plugins/ghe/ghe.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/ghe/ghe.plugin.zsh
@@ -1,9 +1,15 @@
+ensure_docker() {
+  if [ "$(jq -r .auths.\"octofactory.githubapp.com\" ~/.docker/config.json)" = "null" ]; then
+    docker login octofactory.githubapp.com
+  fi
+}
+
 alias src="cd ~/enterprise2;"
 alias sshc="src; ./chroot-ssh.sh"
 alias r="src; ./chroot-stop.sh; ./chroot-reset.sh; ./chroot-cluster-stop.sh; ./chroot-cluster-reset.sh test/cluster.conf; ./chroot-cluster-reset.sh test/cluster-ha.conf; ./chroot-cluster-reset.sh test/cluster-dr.conf;"
 alias b="src; r; ./chroot-build.sh && ./chroot-start.sh && ./chroot-configure.sh"
 alias bc="src; r; ./chroot-build.sh && ./chroot-cluster-start.sh test/cluster.conf"
-alias bf="(export FETCH_DOCKER_IMAGES=1; b)"
-alias bcf="(export FETCH_DOCKER_IMAGES=1; bc)"
+alias bf="ensure_docker; (export FETCH_DOCKER_IMAGES=1; b)"
+alias bcf="ensure_docker; (export FETCH_DOCKER_IMAGES=1; bc)"
 alias bfm="(export ENABLE_MINIO=1; bf)"
 alias bcfm="(export ENABLE_MINIO=1; bcf)"
