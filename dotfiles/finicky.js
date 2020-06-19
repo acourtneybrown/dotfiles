c4dfa276fa392615f5d954e9d2477f34af63d488
Adam Brown <acourtneybrown@gmail.com>
2020-06-18 17:53:44 -0700

    Add config file for Finicky browser picker

---
+++ dotfiles/finicky.js
@@ -0,0 +1,32 @@
+module.exports = {
+  defaultBrowser: "browserosaurus",
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "https" }
+    }
+  ],
+  handlers: [
+    {
+      // Work-related sites
+      match: [
+        /^https:\/\/github\.com\/github.*$/,
+        finicky.matchHostnames([ "team.githubapp.com" ])
+      ],
+      browser: "Google Chrome"
+    },
+    {
+      match: [ /^https:\/\/github.com\/acourtneybrown\/.*$/ ],
+      browser: "Safari"
+    },
+    {
+      match: finicky.matchHostnames([ "applications.zoom.us" ]),
+      browser: "Google Chrome"
+    },
+    {
+      match: finicky.matchHostnames([ "aka.ms" ]),
+      browser: "Google Chrome"
+    }
+  ]
+};
