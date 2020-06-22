3f588c61a14e39d3f145ea5fad5a89d45e66a98a
Adam Brown <acourtneybrown@gmail.com>
2020-06-21 23:39:06 -0700

    Pull secret data from 1Password

--- dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
@@ -1,4 +1,4 @@
 # Include dotdrop zsh completion configuration
 
 # Dotfile management
-alias dotdrop='eval $(grep -v "^#" {{@@ _dotdrop_dotpath @@}}/../.env) {{@@ _dotdrop_dotpath @@}}/../dotdrop.sh'
+alias dotdrop='eval $(grep -v "^#" ${HOME}/.secrets) {{@@ _dotdrop_dotpath @@}}/../dotdrop.sh'
