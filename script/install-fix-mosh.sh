3d10199f877d2f7c78d2498f2db0a9581cd47693
Adam Brown <acourtneybrown@gmail.com>
2020-06-21 21:03:47 -0700

    Add script & LaunchDaemon to ensure mosh is added to Mac firewall at startup

---
+++ script/install-fix-mosh.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Adapted from https://github.com/mobile-shell/mosh/issues/898
+# and https://gist.github.com/sriramkswamy/9cd9887eafb6d4d27a754dcc8d9bd4b1
+
+SOURCE="$(cd `dirname $0` && pwd)"
+
+# create target destination
+mkdir -p "/Users/Shared/.startup"
+
+# put files in correct locations for LaunchDaemon
+sudo cp "${SOURCE}/mosh.sh" "/Users/Shared/.startup/mosh.sh"
+sudo cp "${SOURCE}/com.mosh.plist" "/Library/LaunchDaemons/com.mosh.plist"
+
+# create correct file permissions
+sudo chmod 644 "/Users/Shared/.startup/mosh.sh"
+sudo chmod 644 "/Library/LaunchDaemons/com.mosh.plist"
+
+# create correct file ownerships
+sudo chown root:wheel "/Users/Shared/.startup/mosh.sh"
+sudo chown root:wheel "/Library/LaunchDaemons/com.mosh.plist"
+
+# add mosh launch daemon
+sudo launchctl load -w "/Library/LaunchDaemons/com.mosh.plist"
