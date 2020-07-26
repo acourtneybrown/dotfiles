30e57b43732b70293158e0c418d60b3f60e9a91a
Adam Brown <acourtneybrown@gmail.com>
2020-07-26 15:53:02 -0700

    Extract script to (re-)setup dotdrop's venv

---
+++ script/setup-dotdrop-venv.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+. script/functions
+
+if [ ! -d env ]; then
+  if command -v python3; then
+    python3 -m venv env
+    source env/bin/activate
+    pip install -r dotdrop/requirements.txt
+    deactivate
+  else
+    abort "Python3 must be installed"
+  fi
+fi
