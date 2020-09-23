e8dfeb8b980ab90b373f4bb48fc1289b30dd10c3
Adam Brown <acourtneybrown@gmail.com>
2020-09-23 15:39:32 -0700

    Ensure logins to all docker registries on GHES

--- dotfiles/oh-my-zsh-custom/plugins/ghe/ghe.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/ghe/ghe.plugin.zsh
@@ -1,10 +1,17 @@
 # shellcheck shell=bash
-ensure_docker() {
+function ensure_docker() {
+  host="$1"
   mkdir -p ~/.docker
   touch ~/.docker/config.json
-  if [ -z "$(jq -r .auths.\"octofactory.githubapp.com\" ~/.docker/config.json)" ]; then
-    docker login octofactory.githubapp.com
+  if [ -z "$(jq -r .auths."${host}" ~/.docker/config.json)" ]; then
+    docker login "${host}"
   fi
+}
+
+function ensure_dockers() {
+  ensure_docker "octofactory.githubapp.com"
+  ensure_docker "docker.pkg.github.com"
+  ensure_docker "containers.pkg.github.com"
 }
 
 alias src="cd ~/enterprise2;"
@@ -12,8 +19,8 @@ alias sshc="(src; ./chroot-ssh.sh)"
 alias r="(src; ./chroot-stop.sh; ./chroot-reset.sh; ./chroot-cluster-stop.sh; ./chroot-cluster-reset.sh test/cluster.conf; ./chroot-cluster-reset.sh test/cluster-ha.conf; ./chroot-cluster-reset.sh test/cluster-dr.conf;)"
 alias b="(src; r; ./chroot-build.sh && ./chroot-start.sh && ./chroot-configure.sh)"
 alias bc="(src; r; ./chroot-build.sh && ./chroot-cluster-start.sh test/cluster.conf)"
-alias bf="ensure_docker; (export FETCH_DOCKER_IMAGES=1; b)"
-alias bcf="ensure_docker; (export FETCH_DOCKER_IMAGES=1; bc)"
+alias bf="ensure_dockers; (export FETCH_DOCKER_IMAGES=1; b)"
+alias bcf="ensure_dockers; (export FETCH_DOCKER_IMAGES=1; bc)"
 alias bfm="(export ENABLE_MINIO=1; bf)"
 alias bcfm="(export ENABLE_MINIO=1; bcf)"
 alias stop="(src; ./chroot-stop.sh)"
