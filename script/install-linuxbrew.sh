de56fd5f6f6d85d965889b2e11f5dfaabfba6e6b
Adam Brown <acourtneybrown@gmail.com>
2020-10-06 15:43:32 -0700

    Use homebrew/linuxbrew on Linux

--- script/install-linuxbrew.sh
+++ script/install-linuxbrew.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# From https://docs.brew.sh/Homebrew-on-Linux#alternative-installation
 git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
 mkdir ~/.linuxbrew/bin
 ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
