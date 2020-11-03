f1a7f08cd930eb958cd9da336668ae012a4542a8
Adam Brown <acourtneybrown@gmail.com>
2020-11-03 13:21:00 -0700

    Process jenv-custom plugin only when jenv is installed

--- dotfiles/oh-my-zsh-custom/plugins/jenv-custom/jenv-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/jenv-custom/jenv-custom.plugin.zsh
@@ -1,16 +1,18 @@
 # jenv_sync_versions
-function jenv_sync_versions() {
-  for dir in /Library/Java/JavaVirtualMachines/*; do
-    jenv add "$dir/Contents/Home"
+if [[ $commands[jenv] ]]; then
+  function jenv_sync_versions() {
+    for dir in /Library/Java/JavaVirtualMachines/*; do
+      jenv add "$dir/Contents/Home"
+    done
+  }
+
+  jenvplugins=(maven gradle export)
+
+  for jenvplugin in ${jenvplugins}; do
+    if [[ ! -L "${HOME}/.jenv/plugins/${jenvplugin}" ]]; then
+      jenv enable-plugin "${jenvplugin}"
+    fi
   done
-}
 
-jenvplugins=(maven gradle export)
-
-for jenvplugin in ${jenvplugins}; do
-  if [[ ! -L "${HOME}/.jenv/plugins/${jenvplugin}" ]]; then
-    jenv enable-plugin "${jenvplugin}"
-  fi
-done
-
-RPROMPT='$(jenv_prompt_info)'
+  RPROMPT='$(jenv_prompt_info)'
+fi
