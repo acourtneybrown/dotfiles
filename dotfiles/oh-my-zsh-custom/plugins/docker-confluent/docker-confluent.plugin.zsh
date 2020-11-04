e87092c02db017e56851c645ff55e9779e579f6c
Adam Brown <acourtneybrown@gmail.com>
2020-11-04 10:38:19 -0700

    Update plugins using ~/.secrets to clean up environment

--- dotfiles/oh-my-zsh-custom/plugins/docker-confluent/docker-confluent.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/docker-confluent/docker-confluent.plugin.zsh
@@ -1,17 +1,20 @@
-source ~/.secrets
+# Run in subshell to ensure that info from ~/.secrets cleared
+(
+  source ~/.secrets
 
-function ensure_docker() {
-  local host
-  host="$1"
-  mkdir -p ~/.docker
-  touch ~/.docker/config.json
-  if ! jq -e ".auths.\"${host}\"" ~/.docker/config.json > /dev/null; then
-    docker login \
-      "${host}" \
-      --username ${artifactory_user} \
-      --password ${artifactory_password}
-  fi
-}
+  function ensure_docker() {
+    local host
+    host="$1"
+    mkdir -p ~/.docker
+    touch ~/.docker/config.json
+    if ! jq -e ".auths.\"${host}\"" ~/.docker/config.json > /dev/null; then
+      docker login \
+        "${host}" \
+        --username ${artifactory_user} \
+        --password ${artifactory_password}
+    fi
+  }
 
-ensure_docker "confluent-docker.jfrog.io"
-ensure_docker "confluent-docker-internal-dev.jfrog.io"
+  ensure_docker "confluent-docker.jfrog.io"
+  ensure_docker "confluent-docker-internal-dev.jfrog.io"
+)
--- dotfiles/oh-my-zsh-custom/plugins/helm_2/helm_2.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/helm_2/helm_2.plugin.zsh
@@ -7,11 +7,14 @@ if [[ $commands[helm] ]]; then
   helm init --upgrade > /dev/null
 
   if [[ -z $(helm repo list | grep helm-cloud) ]]; then
-    source ~/.secrets
+    # Run in subshell to ensure that info from ~/.secrets cleared
+    (
+      source ~/.secrets
 
-    helm repo add helm-cloud \
-      https://confluent.jfrog.io/confluent/helm-cloud \
-      --username ${artifactory_user} \
-      --password ${artifactory_password}
+      helm repo add helm-cloud \
+        https://confluent.jfrog.io/confluent/helm-cloud \
+        --username ${artifactory_user} \
+        --password ${artifactory_password}
+    )
   fi
 fi
