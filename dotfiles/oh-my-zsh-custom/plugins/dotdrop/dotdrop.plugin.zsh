8cbe562cd228bfab625a5f084871168db57a8135
Adam Brown <acourtneybrown@gmail.com>
2020-11-06 11:15:15 -0700

    Avoid auto-updating dotdrop submodule
    Back out the update beyond v1.3.6 that broke some templated dotfile

--- dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
@@ -1,4 +1,5 @@
 # Include dotdrop zsh completion configuration
 
 # Dotfile management
+export DOTDROP_AUTOUPDATE=no
 alias dotdrop='eval $(grep -v "^#" ${HOME}/.secrets) {{@@ _dotdrop_dotpath @@}}/../dotdrop.sh'
