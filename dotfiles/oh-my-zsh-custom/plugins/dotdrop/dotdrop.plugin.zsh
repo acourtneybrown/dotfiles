b9be7f0d61728405e484696d35424a3ab714fcb2
Adam Brown <acourtneybrown@gmail.com>
2020-11-09 08:56:34 -0700

    Add function to list available dotdrop versions/tags

--- dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
@@ -4,6 +4,13 @@
 export DOTDROP_AUTOUPDATE=no
 alias dotdrop='eval $(grep -v "^#" ${HOME}/.secrets) {{@@ _dotdrop_dotpath @@}}/../dotdrop.sh'
 
+# dotdrop_versions lists the available tags/releases of the dotdrop submodule
+function dotdrop_versions() {
+  cd "{{@@ _dotdrop_dotpath @@}}"/../dotdrop
+  git fetch --tags
+  git tag --sort=-v:refname -l "${1}*"
+}
+
 # dotdrop_upgrade updates the dotdrop submodule to the specified tag if given,
 # otherwise the latest tagged release
 function dotdrop_upgrade() {
@@ -11,7 +18,7 @@ function dotdrop_upgrade() {
   cd "{{@@ _dotdrop_dotpath @@}}"/../dotdrop
   git fetch --tags
   if [[ $# -lt 1 ]]; then
-    tag=$(git tag --sort=-v:refname -l "${1}*" | head -1)
+    tag=$(dotdrop_versions | head -1)
   else
     tag="$1"
   fi
