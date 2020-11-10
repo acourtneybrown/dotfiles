aa712dc180d4b278abce15910af2278e1aaeb5a2
Adam Brown <acourtneybrown@gmail.com>
2020-11-10 12:01:30 -0700

    Ensure dotdrop_* functions don't change directory

--- dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
@@ -6,21 +6,23 @@ alias dotdrop='eval $(grep -v "^#" ${HOME}/.secrets) {{@@ _dotdrop_dotpath @@}}/
 
 # dotdrop_versions lists the available tags/releases of the dotdrop submodule
 function dotdrop_versions() {
-  cd "{{@@ _dotdrop_dotpath @@}}"/../dotdrop
-  git fetch --tags
-  git tag --sort=-v:refname -l "${1}*"
+  local dotdrop_location
+  dotdrop_location="{{@@ _dotdrop_dotpath @@}}"/../dotdrop
+  git -C ${dotdrop_location} fetch --tags
+  git -C ${dotdrop_location} tag --sort=-v:refname -l "${1}*"
 }
 
 # dotdrop_upgrade updates the dotdrop submodule to the specified tag if given,
 # otherwise the latest tagged release
 function dotdrop_upgrade() {
   local tag
-  cd "{{@@ _dotdrop_dotpath @@}}"/../dotdrop
-  git fetch --tags
+  local dotdrop_location
+  dotdrop_location="{{@@ _dotdrop_dotpath @@}}"/../dotdrop
+  git -C ${dotdrop_location} fetch --tags
   if [[ $# -lt 1 ]]; then
     tag=$(dotdrop_versions | head -1)
   else
     tag="$1"
   fi
-  git checkout "${tag}"
+  git -C ${dotdrop_location} checkout "${tag}"
 }
