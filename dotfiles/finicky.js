e3d64c0a22550dca4b4110baab6165522c95a77b
Adam Brown <acourtneybrown@gmail.com>
2020-12-10 17:26:47 -0700

    Add more Confluent-specific hostnames to Chrome config

--- dotfiles/finicky.js
+++ dotfiles/finicky.js
@@ -14,19 +14,24 @@ module.exports = {
       // Work-related sites
       match: [
         finicky.matchHostnames([
+          "app.firehydrant.io",
           "confluent-tools.datadoghq.com",
           "confluent.askspoke.com",
           "confluent.io",
           "confluent.okta.com",
+          "confluent.slack.com",
           "confluent.zoom.us",
           "confluentinc.atlassian.net",
           "confluentinc.semaphoreci.com",
+          "hire.lever.co",
           "jenkins.confluent.io",
           "jfrog.io",
           "metabase.confluent.io",
           "www.golinks.io",
+          "zoom.us",
         ]),
 
+        /confluent-internal.io/,
         /github.com\/confluentinc/,
         /https:\/\/go\//,
        ],
