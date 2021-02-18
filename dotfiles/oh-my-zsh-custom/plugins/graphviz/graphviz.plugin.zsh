00d1bcf4d03ac722cca2119081add11ccbc33272
Adam Brown <acourtneybrown@gmail.com>
2021-02-18 10:03:23 -0700

    Add dotall function if graphviz is installed

---
+++ dotfiles/oh-my-zsh-custom/plugins/graphviz/graphviz.plugin.zsh
@@ -0,0 +1,15 @@
+if [[ $commands[dot] ]]; then
+  # dotall will run the dot command over all .gv files in a directory & generate image.
+  # by default, it will output png images, but an optional argument can specify a different type
+  function dotall() {
+    local type
+    type=png
+    if [ $# -eq 1 ]; then
+      type="${1}"
+    fi
+
+    ls -1 *.gv | while read file; do
+      dot -T${type} "${file}" >"${file}.${type}"
+    done
+  }
+fi
