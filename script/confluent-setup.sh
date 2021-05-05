e543b490a69bed5ebf8ec654d388efa4e17d0ef7
Adam Brown <acourtneybrown@gmail.com>
2021-05-05 09:57:53 -0700

    Install `confluent-ci-tools` via autopip

--- script/confluent-setup.sh
+++ script/confluent-setup.sh
@@ -10,6 +10,7 @@ ensure_autopip_install
 # Install app that contains pint command and optionally keep it updated daily so you don't have to
 app install release-tools --update daily
 app install tox --update daily
+app install confluent-ci-tools --update daily
 
 if [[ -d ~/.cc-dotfiles ]]; then
   git -C ~/.cc-dotfiles pull origin
