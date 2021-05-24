54c60dcfc08cad53b5697fee01e6c68740158bb2
Adam Brown <acourtneybrown@gmail.com>
2021-05-24 11:59:17 -0700

    Improve functions/aliases for working with different python versions

--- dotfiles/oh-my-zsh-custom/plugins/pypath/pypath.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/pypath/pypath.plugin.zsh
@@ -1,5 +1,5 @@
-# pypath adds the given Homebrew Python version to the end of PATH
-function pypath() {
+# pypath_last adds the given Homebrew Python version to the end of PATH
+function pypath_last() {
   local version
   local bindir
   version="$1"
@@ -13,3 +13,21 @@ function pypath() {
   typeset -U path
   export PATH
 }
+
+# pypath adds the given Homebrew Python version to the front of PATH
+function pypath_first() {
+  local version
+  local bindir
+  version="$1"
+  bindir="/usr/local/opt/python@${version}/bin"
+  if [[ -d ${bindir} ]]; then
+    path=("${bindir}" ${path})
+  else
+    echo "error: ${bindir} does not exist"
+  fi
+
+  typeset -U path
+  export PATH
+}
+
+alias pypath=pypath_first
