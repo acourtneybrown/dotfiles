52aa50d81139cf92229bef0f6d45a9797aa1a354
Adam Brown <acourtneybrown@gmail.com>
2021-05-29 10:31:50 -0700

    Cleanup {home,linux}brew plugins

--- dotfiles/oh-my-zsh-custom/plugins/linuxbrew/linuxbrew.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/linuxbrew/linuxbrew.plugin.zsh
@@ -1,9 +1,8 @@
-# From https://docs.brew.sh/Homebrew-on-Linux#install
 export PATH="${HOME}/.rbenv/bin:${PATH}"
 eval "$(rbenv init - zsh)"
 
-test -d "${HOME}/.linuxbrew" && eval "$("${HOME}/.linuxbrew/bin/brew" shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+[ -d "${HOME}/.linuxbrew" ] && eval "$("${HOME}/.linuxbrew/bin/brew" shellenv)"
+[ -d /home/linuxbrew/.linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # shellcheck disable=SC2034
 typeset -U path
