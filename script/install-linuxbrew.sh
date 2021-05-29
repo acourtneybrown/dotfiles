ff3e71ad5db5aa22da844b6a3b3a3bf2715f942c
Adam Brown <acourtneybrown@gmail.com>
2021-05-28 21:23:46 -0700

    Improve install-linuxbrew.sh

--- script/install-linuxbrew.sh
+++ script/install-linuxbrew.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# TODO: make this work for non-Debian distro?
+apt install ruby
+
 # From https://docs.brew.sh/Homebrew-on-Linux#alternative-installation
-git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
-mkdir ~/.linuxbrew/bin
-ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+if [ ! -d ~/.linuxbrew ]; then
+  git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+  mkdir ~/.linuxbrew/bin
+  ln -s ~/.linuxbrew/Homebrew/bin/brew ~/.linuxbrew/bin
+else
+  git -C ~/.linuxbrew/Homebrew pull
+fi
 eval "$(~/.linuxbrew/bin/brew shellenv)"
