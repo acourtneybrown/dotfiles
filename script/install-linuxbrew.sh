816b0bef74d058e2534453e77852d534e4b92161
Adam Brown <acourtneybrown@gmail.com>
2021-05-28 21:25:46 -0700

    Use sudo to install ruby

--- script/install-linuxbrew.sh
+++ script/install-linuxbrew.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # TODO: make this work for non-Debian distro?
-apt install ruby
+sudo apt install ruby
 
 # From https://docs.brew.sh/Homebrew-on-Linux#alternative-installation
 if [ ! -d ~/.linuxbrew ]; then
