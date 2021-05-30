dfa94e54c0aa505df251a6dfb02eebf1d132ecb8
Adam Brown <acourtneybrown@users.noreply.github.com>
2021-05-29 17:59:57 -0700

    Fix linuxbrew (#28)
    * Improve install-linuxbrew.sh
    
    * Use sudo to install ruby
    
    * Add check to avoid extracting secrets from 1Password
    
    * Make ruby installation quiet
    
    * More work identifying Linux distro & installing linuxbrew
    
    * Install rbenv & ruby 2.6.3 for linuxbrew
    
    * Fix quote
    
    * Install dependencies for rbenv
    
    * Ensure ~/.rbenv in PATH
    
    * Ensure en_US.UTF-8 locale available on Raspbian hosts
    
    * Move RPi locale setup from intall-linuxbrew.sh to os-setup script
    
    * Cleanup {home,linux}brew plugins
    
    * Cleanup linuxbrew plugin path handling

--- dotfiles/oh-my-zsh-custom/plugins/homebrew/homebrew.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/homebrew/homebrew.plugin.zsh
@@ -1,5 +1,6 @@
 [ -d "/usr/local/Homebrew" ] && eval "$("/usr/local/Homebrew/bin/brew" shellenv)"
 [ -d "/opt/homebrew" ] && eval "$("/opt/homebrew/bin/brew" shellenv)"
 
+# shellcheck disable=SC2034
 typeset -U path
 export PATH
--- dotfiles/oh-my-zsh-custom/plugins/linuxbrew/linuxbrew.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/linuxbrew/linuxbrew.plugin.zsh
@@ -1,6 +1,11 @@
-# From https://docs.brew.sh/Homebrew-on-Linux#install
-test -d "${HOME}/.linuxbrew" && eval "$("${HOME}/.linuxbrew/bin/brew" shellenv)"
-test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+# Actually *want* path to be split
+# shellcheck disable=2206
+path=("${HOME}/.rbenv/bin" $path)
+eval "$(rbenv init - zsh)"
 
+[ -d "${HOME}/.linuxbrew" ] && eval "$("${HOME}/.linuxbrew/bin/brew" shellenv)"
+[ -d /home/linuxbrew/.linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+# shellcheck disable=SC2034
 typeset -U path
 export PATH
