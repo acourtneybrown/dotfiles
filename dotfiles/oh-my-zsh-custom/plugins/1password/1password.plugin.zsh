7ccc331578c98b7d7f6c9b293cca4b8e41bc4bf3
Adam Brown <acourtneybrown@gmail.com>
2021-06-03 21:41:53 -0600

    Shell check for 1password plugin

--- dotfiles/oh-my-zsh-custom/plugins/1password/1password.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/1password/1password.plugin.zsh
@@ -2,20 +2,21 @@
 
 alias opsignin='eval $(op signin my)'
 
-# pswd puts the password of the named service into the clipboard
-function pswd() {
-    (( $# < 1 )) && echo "Usage: pswd <service>"
-    local service=$1
-
-    (( ! oploggedin )) && opsignin
-
-    op get item $service | jq -r '.details.fields[] | select(.designation=="password").value' | pbcopy
-
-    ( sleep 5 && op get totp $service | pbcopy
-      sleep 10 && pbcopy < /dev/null 2>&1 & ) &!
-
+function oploggedin() {
+  op list users &>/dev/null
 }
 
-function oploggedin() {
-    op list users &> /dev/null
+# pswd puts the password of the named service into the clipboard
+function pswd() {
+  (($# < 1)) && echo "Usage: pswd <service>"
+  local service=$1
+
+  ! oploggedin && opsignin
+
+  op get item "${service}" | jq -r '.details.fields[] | select(.designation=="password").value' | pbcopy
+
+  (
+    sleep 5 && op get totp "${service}" | pbcopy
+    sleep 10 && pbcopy </dev/null 2>&1 &
+  ) &!
 }
