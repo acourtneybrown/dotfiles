bdda3903b5596bd9d486aae7aad7e996c7fb1a89
Adam Brown <acourtneybrown@gmail.com>
2022-04-10 20:04:22 -0700

    Add jenv-lazy custom plugin to git subtree update script

--- script/update-zsh-plugins.sh
+++ script/update-zsh-plugins.sh
@@ -5,3 +5,4 @@ set -e
 cd "$(dirname "${0}")/.."
 
 git subtree pull --prefix dotfiles/oh-my-zsh-custom/plugins/evalcache https://github.com/mroth/evalcache.git master --squash
+git subtree pull --prefix dotfiles/oh-my-zsh-custom/plugins/jenv-lazy https://github.com/shihyuho/zsh-jenv-lazy.git master --squash
