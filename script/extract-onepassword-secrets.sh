fe6cc2abb5451ec5a028361569a397a5477d0619
Adam Brown <acourtneybrown@gmail.com>
2022-04-12 20:50:09 -0700

    Update to 1Password cli v2

--- script/extract-onepassword-secrets.sh
+++ script/extract-onepassword-secrets.sh
@@ -7,18 +7,7 @@ cd "$(dirname "${0}")/.."
 mkdir -p ~/.ssh
 mkdir -p ~/.gnupg
 
-function onepassword_login() {
-  if ! command -v op >/dev/null; then
-    echo "Install op first!" >&2
-    exit 1
-  fi
-
-  # shellcheck disable=SC2154
-  if [ -z "${OP_SESSION_my}" ]; then
-    echo "Logging into 1Password..."
-    eval "$(op signin my.1password.com acourtneybrown@gmail.com)"
-  fi
-}
+eval "$(op signin)"
 
 function onepassword_get() {
   if [ -f "${HOME}/${2}" ]; then
@@ -26,8 +15,7 @@ function onepassword_get() {
     return
   fi
   echo "Extracting ${2}..."
-  onepassword_login
-  op get document "${1}" --output "${HOME}/${2}"
+  op document get "${1}" --output "${HOME}/${2}"
   chmod 600 "${HOME}/${2}"
 }
 
@@ -51,31 +39,20 @@ if ! [ -f "${HOME}/.secrets" ]; then
     echo "Install jq first!" >&2
     exit 1
   fi
-  onepassword_login
 
   touch "${HOME}/.secrets"
   chmod 600 "${HOME}/.secrets"
 
-  artifactory_host=$(op get item "JFrog Artifactory" - --fields url)
-  artifactory_password=$(op get item "JFrog Artifactory" - --fields "API Key")
-  artifactory_path=$(op get item "JFrog Artifactory" - --fields "Root path")
-  artifactory_user=$(op get item "JFrog Artifactory" - --fields username)
-  gh_cli_token=$(op get item "GitHub" - --fields "gh cli token")
-  github_netrc_token=$(op get item "GitHub" - --fields "Confluent netrc")
-  hub_cli_token=$(op get item "GitHub" - --fields "hub cli token")
-  okta_default_device_token=$(op get item "Okta" - --fields "gimme-aws-creds default device_token")
-  okta_toolsterraform_device_token=$(op get item "Okta" - --fields "gimme-aws-creds ToolsTerraform device_token")
-
   cat >>"${HOME}/.secrets" <<EOF
-artifactory_host=${artifactory_host}
-artifactory_password=${artifactory_password}
-artifactory_path=${artifactory_path}
-artifactory_user=${artifactory_user}
-gh_cli_token=${gh_cli_token}
-github_netrc_token=${github_netrc_token}
-hub_cli_token=${hub_cli_token}
-okta_default_device_token=${okta_default_device_token}
-okta_toolsterraform_device_token=${okta_toolsterraform_device_token}
+artifactory_host=$(op item get "JFrog Artifactory" --format json | jq '.urls[0].href')
+artifactory_password=$(op item get "JFrog Artifactory" --fields "API Key")
+artifactory_path=$(op item get "JFrog Artifactory" --fields "Root path")
+artifactory_user=$(op item get "JFrog Artifactory" --fields username)
+gh_cli_token=$(op item get "GitHub" --fields "gh cli token")
+github_netrc_token=$(op item get "GitHub" --fields "Confluent netrc")
+hub_cli_token=$(op item get "GitHub" --fields "hub cli token")
+okta_default_device_token=$(op item get "Okta" --fields "gimme-aws-creds default device_token")
+okta_toolsterraform_device_token=$(op item get "Okta" --fields "gimme-aws-creds ToolsTerraform device_token")
 EOF
 fi
 
