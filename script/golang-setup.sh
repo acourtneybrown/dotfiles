3cc55bba70a0e78ff02c77c64357ba3508dd11ec
Adam Brown <acourtneybrown@gmail.com>
2022-05-04 21:13:51 -0700

    Use go install vs go get -u

--- script/golang-setup.sh
+++ script/golang-setup.sh
@@ -11,10 +11,10 @@ if ! command -v go; then
   abort "Go must be installed"
 fi
 
-go get -u github.com/codejanovic/gordon
-go get -u github.com/go-delve/delve/cmd/dlv
-go get -u github.com/gogo/protobuf/protoc-gen-gogofast
-go get -u golang.org/x/lint/golint
-go get -u golang.org/x/tools/cmd/godoc
-go get -u golang.org/x/tools/cmd/goimports
-go get -u rsc.io/goversion
+go install github.com/codejanovic/gordon
+go install github.com/go-delve/delve/cmd/dlv
+go install github.com/gogo/protobuf/protoc-gen-gogofast
+go install golang.org/x/lint/golint
+go install golang.org/x/tools/cmd/godoc
+go install golang.org/x/tools/cmd/goimports
+go install rsc.io/goversion
