5ffe1df40a443efa68c254ef785ccdfd37451b57
Adam Brown <acourtneybrown@users.noreply.github.com>
2022-10-31 17:26:17 -0700

    Changes necessary to install & build with Bazel (#48)
    * Changes necessary to install & build with Bazel
    
    * Sort cask installs
    
    * Added git alias needed for JetBrains https settings repos

---
+++ dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
@@ -0,0 +1,7 @@
+# shellcheck disable=SC2148
+
+if [ -f ~/.config/coursier/credentials.properties ]; then
+  export COURSIER_CREDENTIALS=~/.config/coursier/credentials.properties
+fi
+
+export GOPRIVATE="github.com/confluentinc/*"
--- dotfiles/oh-my-zsh-custom/plugins/setopts/setopts.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/setopts/setopts.plugin.zsh
@@ -1,8 +1,7 @@
 # shellcheck disable=SC2148
 
 setopt APPEND_HISTORY         # append to history file
-setopt CORRECT                # Try  to  correct the spelling of commands.
-setopt CORRECT_ALL            # Try to correct the spelling of all arguments in a line.
+setopt CORRECT                # Try to correct the spelling of commands.
 setopt HIST_EXPIRE_DUPS_FIRST # Expire a duplicate event first when trimming history.
 setopt HIST_FIND_NO_DUPS      # Do not display a previously found event.
 setopt HIST_IGNORE_DUPS       # Do not record an event that was just recorded again.
@@ -11,3 +10,5 @@ setopt HIST_REDUCE_BLANKS     # Remove superfluous blanks
 setopt INC_APPEND_HISTORY     # Write to the history file immediately, not when the shell exits.
 setopt NO_CASE_GLOB           # Make  globbing  (filename  generation) insensitive to case.
 setopt SHARE_HISTORY          # Share history between all sessions.
+
+# setopt CORRECT_ALL            # Try to correct the spelling of all arguments in a line.
