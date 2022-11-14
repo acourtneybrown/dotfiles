77ca24c1b55e747e2a7f3aed06a6f5a4f14c00fc
Adam Brown <acourtneybrown@gmail.com>
2022-11-14 13:43:44 -0700

    Update dotdrop.sh from submodule changes & shellcheck

--- dotdrop.sh
+++ dotdrop.sh
@@ -18,22 +18,32 @@ fi
 args=("$@")
 cur=$(dirname "$(${rl} "${0}")")
 opwd=$(pwd)
-cfg="${cur}/config.yaml"
-sub="dotdrop"
+# cfg="${cur}/config.yaml"
+# sub="dotdrop"
 
 # pivot
 cd "${cur}" || { echo "Directory \"${cur}\" doesn't exist, aborting." && exit 1; }
+
 # init/update the submodule
 if [ "${DOTDROP_AUTOUPDATE-yes}" = yes ]; then
   git submodule update --init --recursive
   git submodule update --remote dotdrop
 fi
-# launch dotdrop
+
 # shellcheck disable=SC1091
 source env/bin/activate
+
+# check python executable
+# pybin="python3"
+# hash ${pybin} 2>/dev/null || pybin="python"
+# [[ "`${pybin} -V 2>&1`" =~ "Python 3" ]] || { echo "install Python 3" && exit 1; }
+
+# launch dotdrop
 PYTHONPATH=dotdrop python3 -m dotdrop.dotdrop "${args[@]}"
 ret="$?"
+
 # pivot back
 cd "${opwd}" || { echo "Directory \"${opwd}\" doesn't exist, aborting." && exit 1; }
+
 # exit with dotdrop exit code
 exit ${ret}
