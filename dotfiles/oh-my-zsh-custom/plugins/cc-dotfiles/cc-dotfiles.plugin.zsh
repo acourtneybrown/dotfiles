8f677d15a4e23f7be6158f2bf2881519f61ffee4
Adam Brown <acourtneybrown@users.noreply.github.com>
2022-11-18 13:07:30 -0700

    Install cc-dotfiles via "fake" rule & clean up zsh custom plugin (#53)
    * Install cc-dotfiles via "fake" rule & clean up zsh custom plugin
    
    * Fix spellcheck errors
    
    * Comment out flock installation as no longer needed with cc-dotfiles changes
    
    * Allow bootstrap script to add add'l dotdrop profiles to new entry
    
    * Warn if gcloud not installed with confluent-artifacts plugin

--- dotfiles/oh-my-zsh-custom/plugins/cc-dotfiles/cc-dotfiles.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/cc-dotfiles/cc-dotfiles.plugin.zsh
@@ -6,28 +6,21 @@ if [[ -n "${INTELLIJ_ENVIRONMENT_READER}" ]]; then
 fi
 
 if [[ -d "${HOME}/.cc-dotfiles" ]]; then
-  # Ensure that the auto-update behavior is only run by one shell at a time
-  flock "${HOME}/.cc-dotfiles" bash -c "source \${HOME}/.cc-dotfiles/caas.sh"
+  export CC_AUTO_UPDATE=false
+  export OKTA_DEVICE_ID="{{@@ env['okta_default_device_token'] @@}}"
 
-  # shellcheck disable=SC1090,SC1091
+  # shellcheck disable=SC1091,SC1090
   source "${HOME}/.cc-dotfiles/caas.sh"
 
-  function cc-dotfiles_update() {
-    git -C "${HOME}/.cc-dotfiles" pull origin
-  }
-
-  function cc-dotfiles_alpha() {
+  function cc-dotfiles-alpha() {
     export CC_DOTFILES_ALPHA=true
+    # shellcheck disable=SC1091,SC1090
+    source "${HOME}/.cc-dotfiles/caas.sh"
   }
 
-  function cc-dotfiles_beta() {
+  function cc-dotfiles-beta() {
     export CC_DOTFILES_BETA=true
+    # shellcheck disable=SC1091,SC1090
+    source "${HOME}/.cc-dotfiles/caas.sh"
   }
-
-  function cc-dotfiles_ga() {
-    unset CC_DOTFILES_ALPHA
-    unset CC_DOTFILES_BETA
-  }
-
-  export OKTA_DEVICE_ID=uft62yn7znTudH1Nr357
 fi
