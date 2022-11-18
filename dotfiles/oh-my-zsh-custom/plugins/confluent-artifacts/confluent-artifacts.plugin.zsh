724c0c5afc5104db266284fada26a01fab2d4aa2
Adam Brown <acourtneybrown@gmail.com>
2022-11-17 22:55:45 -0700

    Warn if gcloud not installed with confluent-artifacts plugin

--- dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
@@ -29,9 +29,13 @@ alias twine_login='gimme-aws-creds --profile devprod-prod && aws codeartifact lo
 alias pip-login=pip_login
 alias twine-login=twine_login
 
-if ! gcloud components list --only-local-state 2>/dev/null | grep -q gke-gcloud-auth-plugin; then
-  gcloud components install gke-gcloud-auth-plugin
+if command -v gcloud >/dev/null; then
+  if ! gcloud components list --only-local-state 2>/dev/null | grep -q gke-gcloud-auth-plugin; then
+    gcloud components install gke-gcloud-auth-plugin
+  else
+    gcloud components update
+  fi
+  export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 else
-  gcloud components update
+  echo "gcloud tool not installed"
 fi
-export USE_GKE_GCLOUD_AUTH_PLUGIN=True
