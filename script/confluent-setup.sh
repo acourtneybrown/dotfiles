c96d6c05f7337efabff73bdae80956e0b4e8d977
Adam Brown <acourtneybrown@gmail.com>
2022-11-19 00:17:25 -0700

    Ensure that pyenv is enabled before using with pipx_install

--- script/confluent-setup.sh
+++ script/confluent-setup.sh
@@ -7,6 +7,7 @@ cd "$(dirname "${0}")/.."
 
 ensure_brewfile_installed Brewfile.confluent
 
+enable_pyenv
 pipx_install 3.8 confluent-release-tools
 pipx_install 3.9 confluent-ci-tools
 pipx_install 3.11 ansible-hostmanager bump2version gimme-aws-creds gql tox
