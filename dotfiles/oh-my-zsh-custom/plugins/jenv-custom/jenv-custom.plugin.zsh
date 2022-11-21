48bb0a1bc2ef81100fa7799ab9713c9846a16802
Adam Brown <acourtneybrown@users.noreply.github.com>
2022-11-20 19:19:52 -0700

    Update bootstrap-* to install tools versions via *env (#64)
    * Rework adding new "include" profiles to dotdrop's config
    
    * Update bootstrap-* to install tools versions via *env
    
    * Remove unnecessary "missing" value
    
    * Fix wrong function call
    
    * Factor out pyenv, goenv commonality into functions
    
    * Fix whitespace in goenv-based versions
    
    * Filter out stdout of pyenv/goenv install
    
    * Leave the dotdrop/ submodule at the current version during install
    
    * Remove ensure_profile_exists, because ensure_profile_includes is sufficient
    
    * Make pyenv & goenv versions local to ~/<profile> & set global versions in bootstrap script
    
    * Add comments about ensure_*_version stdout
    
    * Move jenv sync & plugin enabling code from Zsh plugin to shared functions
    
    * Add back shellcheck comment

--- dotfiles/oh-my-zsh-custom/plugins/jenv-custom/jenv-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/jenv-custom/jenv-custom.plugin.zsh
@@ -2,22 +2,6 @@
 
 # shellcheck disable=SC2154
 if [[ ${commands[jenv]} ]]; then
-  # jenv_sync_versions makes sure that all installed JDKs are added to jenv
-  function jenv_sync_versions() {
-    for dir in /Library/Java/JavaVirtualMachines/*; do
-      jenv add "$dir/Contents/Home"
-    done
-  }
-
-  jenvplugins=(maven gradle export)
-
-  # shellcheck disable=SC2128
-  for jenvplugin in ${jenvplugins}; do
-    if [[ ! -L "${HOME}/.jenv/plugins/${jenvplugin}" ]]; then
-      jenv enable-plugin "${jenvplugin}"
-    fi
-  done
-
   # shellcheck disable=SC2016
   RPROMPT+=' j$(jenv_prompt_info)'
 fi
