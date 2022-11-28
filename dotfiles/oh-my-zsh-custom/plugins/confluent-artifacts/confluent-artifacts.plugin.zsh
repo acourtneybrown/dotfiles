e74ce5e8e9041c63fa622dec75bad35070665bb8
Adam Brown <acourtneybrown@gmail.com>
2022-11-28 16:55:09 -0700

    Swap from Jfrog to CodeArtifact for Maven dependencies

--- dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
@@ -1,57 +1,45 @@
 # shellcheck disable=SC2148
 
-# function artifactory_user() {
-#   op item get "JFrog Artifactory" --format json | jq -r '.urls[0].href'
-# }
-
-# function artifactory_password() {
-#   op item get "JFrog Artifactory" --fields "API Key"
-# }
-
-# function ensure_docker() {
-#   local host
-
-#   host="${1}"
-#   mkdir -p "${HOME}/.docker"
-#   touch "${HOME}/.docker/config.json"
-#   if ! jq -e ".auths.\"${host}\"" "${HOME}/.docker/config.json" >/dev/null; then
-
-#     # shellcheck disable=SC2154
-#     docker login \
-#       "${host}" \
-#       --username "$(artifactory_user)" \
-#       --password "$(artifactory_password)"
-#   fi
-# }
-
-# ensure_docker "confluent-docker.jfrog.io"
-# ensure_docker "confluent-docker-internal-dev.jfrog.io"
-
-# # shellcheck disable=SC2154
-# if [[ ${commands[helm]} ]]; then
-#   if ! helm repo list | grep -q helm-cloud; then
-#     # Run in subshell to ensure that secrets aren't retained
-#     (
-#       # shellcheck disable=SC2154
-#       helm repo add helm-cloud \
-#         https://confluent.jfrog.io/confluent/helm-cloud \
-#         --username "$(artifactory_user)" \
-#         --password "$(artifactory_password)"
-#     )
-#   fi
-# fi
-
-# shellcheck disable=SC1083
-export ARTIFACTORY_USERNAME={{@@ artifactory_user @@}}
-# shellcheck disable=SC1083
-export ARTIFACTORY_PASSWORD={{@@ artifactory_password @@}}
-
 alias ecr_login="gimme-aws-creds --profile devprod-prod && aws ecr get-login-password --region us-west-2 --profile devprod-prod | docker login --username AWS --password-stdin 519856050701.dkr.ecr.us-west-2.amazonaws.com"
 alias pip_login='gimme-aws-creds --profile devprod-prod && aws codeartifact login --profile devprod-prod --tool pip --domain confluent --domain-owner 519856050701 --region us-west-2 --repository pypi'
 alias twine_login='gimme-aws-creds --profile devprod-prod && aws codeartifact login --profile devprod-prod --tool twine --domain confluent --domain-owner 519856050701 --region us-west-2 --repository pypi-internal'
 alias ecr-login=ecr_login
 alias pip-login=pip_login
 alias twine-login=twine_login
+function maven_login() {
+  local CA_TOKEN_FILE=~/.config/confluent/codeartifact_auth_token
+  local force=false
+  if [[ "$1" == "-f" ]]; then
+    force=true
+  fi
+
+  if [[ "$force" == "true" ]]; then
+    echo "Forcing login"
+    rm -f ${CA_TOKEN_FILE}
+  fi
+
+  if [[ -f ${CA_TOKEN_FILE} ]] && ! [[ $(find "${CA_TOKEN_FILE}" -mmin +660 -print) ]]; then
+    echo "Skipping token generation. File ${CA_TOKEN_FILE} exists and is newer than 11 hours. Use -f to force login."
+    echo "Exporting CODEARTIFACT_AUTH_TOKEN"
+    # shellcheck disable=SC2155
+    export CODEARTIFACT_AUTH_TOKEN=$(cat ${CA_TOKEN_FILE})
+    return
+  fi
+
+  if [[ ! -f ${CA_TOKEN_FILE} ]]; then
+    touch ${CA_TOKEN_FILE}
+  fi
+
+  chmod 600 ${CA_TOKEN_FILE}
+  gimme-aws-creds --profile devprod-prod
+  # shellcheck disable=SC2155
+  export CODEARTIFACT_AUTH_TOKEN=$(aws codeartifact get-authorization-token --domain confluent --region us-west-2 --query authorizationToken --output text --profile devprod-prod)
+  sed -i'.bak' "s/mavenPassword=.*/mavenPassword=$CODEARTIFACT_AUTH_TOKEN/" ~/.gradle/gradle.properties
+  sed -i'.bak' "s/cflt.password=.*/cflt.password=${CODEARTIFACT_AUTH_TOKEN}/" ~/.config/coursier/credentials.properties
+  mkdir -p ~/.config/confluent
+  echo "${CODEARTIFACT_AUTH_TOKEN}" >${CA_TOKEN_FILE}
+}
+alias maven-login=maven_login
 
 if command -v gcloud >/dev/null; then
   if ! gcloud components list --only-local-state 2>/dev/null | grep -q gke-gcloud-auth-plugin; then
