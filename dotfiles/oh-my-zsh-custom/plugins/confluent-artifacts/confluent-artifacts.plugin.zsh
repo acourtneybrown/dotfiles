e28812cf91eb53f8a5f76e4f7af6af46d7695cf0
Adam Brown <acourtneybrown@users.noreply.github.com>
2022-11-30 10:49:48 -0700

    Extract gcloud-custom plugin (#81)

--- dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
@@ -40,14 +40,3 @@ function maven_login() {
   echo "${CODEARTIFACT_AUTH_TOKEN}" >${CA_TOKEN_FILE}
 }
 alias maven-login=maven_login
-
-if command -v gcloud >/dev/null; then
-  if ! gcloud components list --only-local-state 2>/dev/null | grep -q gke-gcloud-auth-plugin; then
-    gcloud components install gke-gcloud-auth-plugin
-  else
-    gcloud components update
-  fi
-  export USE_GKE_GCLOUD_AUTH_PLUGIN=True
-else
-  echo "gcloud tool not installed"
-fi
