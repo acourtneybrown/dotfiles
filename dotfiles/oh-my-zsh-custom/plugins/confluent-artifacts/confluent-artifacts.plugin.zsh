2dfe77cd7707b41a0aa228fdf3a1606a08651f14
Adam Brown <acourtneybrown@users.noreply.github.com>
2022-11-30 10:52:29 -0700

    Add aliases for NPM login to Code Artifact (#82)

--- dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/confluent-artifacts/confluent-artifacts.plugin.zsh
@@ -3,9 +3,8 @@
 alias ecr_login="gimme-aws-creds --profile devprod-prod && aws ecr get-login-password --region us-west-2 --profile devprod-prod | docker login --username AWS --password-stdin 519856050701.dkr.ecr.us-west-2.amazonaws.com"
 alias pip_login='gimme-aws-creds --profile devprod-prod && aws codeartifact login --profile devprod-prod --tool pip --domain confluent --domain-owner 519856050701 --region us-west-2 --repository pypi'
 alias twine_login='gimme-aws-creds --profile devprod-prod && aws codeartifact login --profile devprod-prod --tool twine --domain confluent --domain-owner 519856050701 --region us-west-2 --repository pypi-internal'
-alias ecr-login=ecr_login
-alias pip-login=pip_login
-alias twine-login=twine_login
+alias npm_login='gimme-aws-creds --profile devprod-prod && aws codeartifact login --tool npm --repository npm-internal --domain confluent --domain-owner 519856050701 --region us-west-2 && export CODEARTIFACT_AUTH_TOKEN=`aws codeartifact get-authorization-token --domain confluent --domain-owner 519856050701 --region us-west-2 --query authorizationToken --output text`'
+
 function maven_login() {
   local CA_TOKEN_FILE=~/.config/confluent/codeartifact_auth_token
   local force=false
@@ -39,4 +38,9 @@ function maven_login() {
   sed -i'.bak' "s/cflt.password=.*/cflt.password=${CODEARTIFACT_AUTH_TOKEN}/" "$(readlink -f ~/.config/coursier/credentials.properties)"
   echo "${CODEARTIFACT_AUTH_TOKEN}" >${CA_TOKEN_FILE}
 }
+
+alias ecr-login=ecr_login
+alias pip-login=pip_login
+alias twine-login=twine_login
+alias npm-login=npm_login
 alias maven-login=maven_login
