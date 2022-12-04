78b8aead8a1cd894dfc56a05939611a8a361ea29
Adam Brown <acourtneybrown@gmail.com>
2022-12-04 15:04:32 -0700

    Update to code_artifact:: library

--- script/lib/profile.sh
+++ script/lib/profile.sh
@@ -82,7 +82,7 @@ function profile::confluent_after() {
   source "${HOME}/.cc-dotfiles/include/devprod-ga/code-artifact.sh"
   export PATH="${HOME}/.local/bin:${PATH}"
   gimme-aws-creds --profile devprod-prod # force initial login
-  devprod::pip_login
+  code_artifact::pip_login
 
   profile::pipx_install 3.8 confluent-release-tools
   profile::pipx_install 3.9 confluent-ci-tools
