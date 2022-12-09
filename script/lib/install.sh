036f8ac0386d91555f908ceafb44b6f7c746a795
Adam Brown <acourtneybrown@gmail.com>
2022-12-08 20:45:06 -0700

    Update xcode installation & move into function

--- script/lib/install.sh
+++ script/lib/install.sh
@@ -39,6 +39,33 @@ function install::linux_updates() {
   esac
 }
 
+function install::install_xcode() {
+  if ! [ -f "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
+    log "Installing the Xcode Command Line Tools:"
+    CLT_PLACEHOLDER="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    sudo_askpass touch "$CLT_PLACEHOLDER"
+
+    CLT_PACKAGE=$(softwareupdate -l |
+      grep -B 1 "Command Line Tools" |
+      awk -F"*" '/^ *\*/ {print $2}' |
+      sed -e 's/^ *Label: //' -e 's/^ *//' |
+      sort -V |
+      tail -n1)
+    sudo_askpass softwareupdate -i "$CLT_PACKAGE"
+    sudo_askpass rm -f "$CLT_PLACEHOLDER"
+    if ! [ -f "/Library/Developer/CommandLineTools/usr/bin/git" ]; then
+      if [ -n "$STRAP_INTERACTIVE" ]; then
+        echo "Requesting user install of Xcode Command Line Tools:"
+        xcode-select --install
+      else
+        abort "Run 'xcode-select --install' to install the Xcode Command Line Tools."
+      fi
+    fi
+  fi
+
+  install::xcode_license
+}
+
 # xcode_license checks if the Xcode license is agreed to and agree if not.
 function install::xcode_license() {
   if /usr/bin/xcrun clang 2>&1 | grep -q license; then
