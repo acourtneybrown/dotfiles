1ddfd0361f0097c7b7d7ffd30905004cb3ef873c
Adam Brown <acourtneybrown@gmail.com>
2022-12-14 19:11:56 -0700

    Rename plugin script name
    Added link to page describing the recommended tool.

---
+++ dotfiles/oh-my-zsh-custom/plugins/modern-unix/modern-unix.plugin.zsh
@@ -0,0 +1,36 @@
+# shellcheck disable=SC2148
+
+autoload -U colors && colors
+
+function modern-unix::message() {
+  # shellcheck disable=SC2154
+  echo "${fg[red]}${bg[white]}Use ${1} - ${fg[blue]}${2}${reset_color}" >&2
+}
+
+function ls() {
+  local args=(exa https://github.com/ogham/exa)
+  modern-unix::message "${args[@]}"
+  /bin/ls "${@}"
+  modern-unix::message "${args[@]}"
+}
+
+function cat() {
+  local args=(bat https://github.com/sharkdp/bat)
+  modern-unix::message "${args[@]}"
+  /bin/cat "${@}"
+  modern-unix::message "${args[@]}"
+}
+
+function diff() {
+  local args=(delta https://github.com/dandavison/delta)
+  modern-unix::message "${args[@]}"
+  /usr/bin/diff "${@}"
+  modern-unix::message "${args[@]}"
+}
+
+function find() {
+  local args=(fd https://github.com/sharkdp/fd)
+  modern-unix::message "${args[@]}"
+  /usr/bin/find "${@}"
+  modern-unix::message "${args[@]}"
+}
