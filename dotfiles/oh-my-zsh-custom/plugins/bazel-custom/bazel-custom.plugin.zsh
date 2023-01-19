771276aa319ec66dc8569e4cbbd8a08ad0f6d2b7
Adam Brown <acourtneybrown@gmail.com>
2023-01-19 15:55:48 -0700

    Add clean_bazel_cache function to bazel-custom plugin

--- dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
@@ -1,7 +1,13 @@
 # shellcheck disable=SC2148
 
-if [[ -f ~/.config/coursier/credentials.properties ]]; then
+if [[ -f ${HOME}/.config/coursier/credentials.properties ]]; then
   export COURSIER_CREDENTIALS=~/.config/coursier/credentials.properties
 fi
 
 export GOPRIVATE="github.com/confluentinc/*"
+
+# clean_bazel_cache deletes cache file older than the time specified (60d by default)
+function clean_bazel_cache() {
+  local OLDER_THAN=${1:-60d}
+  find "${HOME}/.cache/bazel" -type f -atime "+${OLDER_THAN}" -delete
+}
