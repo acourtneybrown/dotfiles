74e5f8085045b023e5a37de56848b78995ceccfc
Adam Brown <acourtneybrown@gmail.com>
2023-01-22 16:59:02 -0700

    Rename clean_bazel_cache to have bazel prefix (& alias with dashes)

--- dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
@@ -6,8 +6,9 @@ fi
 
 export GOPRIVATE="github.com/confluentinc/*"
 
-# clean_bazel_cache deletes cache file older than the time specified (60d by default)
-function clean_bazel_cache() {
+# bazel_clean_cache deletes cache file older than the time specified (60d by default)
+function bazel_clean_cache() {
   local OLDER_THAN=${1:-60d}
   find "${HOME}/.cache/bazel" -type f -atime "+${OLDER_THAN}" -delete
 }
+alias bazel-clean-cache=bazel_clean_cache
