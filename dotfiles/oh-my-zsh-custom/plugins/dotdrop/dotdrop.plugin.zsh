9c9ce29e9618e48290f8ba09f69309b3b3322024
Adam Brown <acourtneybrown@gmail.com>
2023-01-21 21:20:53 -0700

    Remove the dotdrop() function & export DOTDROP_CONFIG

--- dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/dotdrop/dotdrop.plugin.zsh
@@ -2,7 +2,4 @@
 
 # Include dotdrop zsh completion configuration
 fpath+=$(pwd)
-
-function dotdrop() {
-  "$(brew --prefix)/bin/dotdrop" --cfg "{{@@ _dotdrop_dotpath @@}}/../config.yaml" "${@}"
-}
+export DOTDROP_CONFIG="{{@@ _dotdrop_dotpath @@}}/../config.yaml"
