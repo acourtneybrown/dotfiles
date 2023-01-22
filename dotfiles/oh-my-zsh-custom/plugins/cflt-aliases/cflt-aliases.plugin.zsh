5a4f65b71faebd77a9ffdb3a35b111c7706156bf
Adam Brown <acourtneybrown@gmail.com>
2023-01-21 21:28:32 -0700

    Replace ~ with ${HOME} & use `ct` alias

--- dotfiles/oh-my-zsh-custom/plugins/cflt-aliases/cflt-aliases.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/cflt-aliases/cflt-aliases.plugin.zsh
@@ -1,8 +1,8 @@
 # shellcheck disable=SC2148
 
-alias ct="cd ~/confluent/temp"
+alias ct='cd ${HOME}/confluent/temp'
 function ctcl() {
-  cd ~/confluent/temp || return
+  ct || return
   gcl "ghc:${1}"
 
   # shellcheck disable=SC2164
