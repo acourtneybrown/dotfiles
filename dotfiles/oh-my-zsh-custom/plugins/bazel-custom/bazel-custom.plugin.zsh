9fa93806fffa52da47cdc8f4970a5d3a960f42ca
Adam Brown <acourtneybrown@gmail.com>
2023-03-21 20:26:30 -0700

    Add sri-hash function & alias

--- dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
@@ -12,3 +12,17 @@ function bazel_clean_cache() {
   find "${HOME}/.cache/bazel" -type f -atime "+${OLDER_THAN}" -delete
 }
 alias bazel-clean-cache=bazel_clean_cache
+
+# sri_hash produces the Subresource Integrity hash for a given file
+# This value is needed for archive_override w/ Bzlmod
+# For details, see https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity
+function sri_hash() {
+  if [[ ${#} -ne 2 ]]; then
+    echo "Usage:"
+    echo "${0} <filename> <hash algo>"
+    return 1
+  fi
+
+  openssl dgst "-${2}" -binary <"${1}" | openssl base64 -A
+}
+alias sri-hash=sri_hash
