e62654117cb5c8cb3afec9ad171f09ab5f4eafa7
Adam Brown <acourtneybrown@gmail.com>
2023-03-30 10:37:59 -0700

    Add bazel_deps/bazel-deps commands to output the Bazel dependency graph

--- dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/bazel-custom/bazel-custom.plugin.zsh
@@ -11,4 +11,13 @@ function bazel_clean_cache() {
   local OLDER_THAN=${1:-60d}
   find "${HOME}/.cache/bazel" -type f -atime "+${OLDER_THAN}" -delete
 }
+
+# bazel_deps outputs the dependency graph for a given Bazel target (default //...)
+function bazel_deps() {
+  local target
+  target="${1-//...}"
+  bazel query --notool_deps --noimplicit_deps "deps(${target})" --output graph
+}
+
 alias bazel-clean-cache=bazel_clean_cache
+alias bazel-deps=bazel_deps
