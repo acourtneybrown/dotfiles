2e98ae9893eb0a33bf08231bdf82095135fd69b3
Adam Brown <acourtneybrown@users.noreply.github.com>
2023-09-11 17:16:04 -0700

    acourtneybrown/alfred 1p containers (#165)
    * Install hiddenbar
    
    * Install bartender
    
    * Revert "Install hiddenbar"
    
    This reverts commit 080f34f145d22c1d9e972dafd314e38af340aa4d.
    
    * Allow certain 1p links to open in Me container via Alfred
    
    * Remove testing code
    
    * Ensure firefox_me_ids are always sorted in same order
    
    * Remove finicky.log lines

--- dotfiles/finicky.js
+++ dotfiles/finicky.js
@@ -6,6 +6,37 @@ function openInFirefoxContainer(containerName, urlString) {
   )}`;
 }
 
+function contains1pId(urlSearch) {
+  for (const id of meContainerIds) {
+    if (urlSearch.includes(`${id}=${id}`)) {
+      return true
+    }
+  }
+  return false
+}
+
+function remove1pId(urlString) {
+  result = urlString
+  for (const regex of meUrlSearchRegex) {
+    result = result.replace(regex, "")
+  }
+  return result
+}
+
+const noContainerHosts = new Set(['duckduckgo.com', 'www.google.com', 'www.amazon.com', 'wikipedia.org'])
+const bundleIdsFor1p = new Set(["com.agilebits.onepassword7", "com.1password.1password"])
+const bundleIdsForAlfred = new Set(["com.runningwithcrayons.Alfred"])
+const meContainerIds = [
+  {%@@ for id in firefox_me_ids.split() @@%}
+    {{@@ id @@}},
+  {%@@ endfor @@%}
+]
+const meUrlSearchRegex = [
+  {%@@ for id in firefox_me_ids.split() @@%}
+    new RegExp("[?&]?" + {{@@ id @@}} + "=" + {{@@ id @@}}),
+  {%@@ endfor @@%}
+]
+
 module.exports = {
   defaultBrowser: "Firefox",
   rewrite: [
@@ -93,14 +124,19 @@ module.exports = {
 
         "https://www.amazon.com/alexa-privacy/apd/rvh",
 
-        ({ opener }) => {
-          // finicky.log(opener.bundleId);
-          return opener.bundleId
-            && (opener.bundleId === "com.agilebits.onepassword7" || opener.bundleId === "com.1password.1password")
+        ({ opener, url }) => {
+          if (bundleIdsFor1p.has(opener.bundleId)) {
+            return true
+          }
+
+          if (bundleIdsForAlfred.has(opener.bundleId)) {
+            return !noContainerHosts.has(url.host) || contains1pId(url.search)
+          }
+          return false
         },
       ],
       url: ({ urlString }) => {
-        return openInFirefoxContainer("Me", urlString);
+        return openInFirefoxContainer("Me", remove1pId(urlString));
       },
       browser: "Firefox",
     },
