ae8a3f5a4a48781d25f015ac478fbcb48e4c135a
Adam Brown <acourtneybrown@gmail.com>
2023-11-12 20:00:09 -0700

    Add PyCharm to path

--- dotfiles/oh-my-zsh-custom/plugins/path/path.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/path/path.plugin.zsh
@@ -1,7 +1,15 @@
 # shellcheck disable=SC2148
 
 # paths in reverse order of the expectation
-paths=(/usr/local/sbin /usr/local/bin "${HOME}/go/bin" "${HOME}/bin" /Applications/GoLand.app/Contents/MacOS /Applications/IntelliJ\ IDEA.app/Contents/MacOS)
+paths=(
+  /usr/local/sbin
+  /usr/local/bin
+  "${HOME}/go/bin"
+  "${HOME}/bin"
+  "/Applications/GoLand.app/Contents/MacOS"
+  "/Applications/IntelliJ IDEA.app/Contents/MacOS"
+  "/Applications/PyCharm.app/Contents/MacOS/"
+)
 
 # ZSH "properly" handles the array (vs. bash)
 # shellcheck disable=SC2128
