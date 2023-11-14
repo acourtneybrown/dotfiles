6bafc8376329036c7d10ee3bd8a9f8032e138c8f
Adam Brown <acourtneybrown@gmail.com>
2023-11-14 09:29:43 -0700

    Remove inadvertent duplication of Confluent-related finicky.js config

--- dotfiles/finicky.js
+++ dotfiles/finicky.js
@@ -49,57 +49,6 @@ module.exports = {
   ],
   handlers: [
     {
-      // Confluent-related sites
-      match: [
-        finicky.matchHostnames([
-          "a.goodtime.io",
-          "app.firehydrant.io",
-          "app.geekbot.com",
-          "app.glean.com",
-          "circleci.com",
-          "confluent-tools.datadoghq.com",
-          "confluent.askspoke.com",
-          "confluent.okta.com",
-          "confluent.slack.com",
-          "confluentinc.atlassian.net",
-          "device.sso.us-west-2.amazonaws.com",
-          "docs.google.com",
-          "hire.lever.co",
-          "ironcladapp.com",
-          "jenkins.confluent.io",
-          "lookerstudio.google.com",
-          "metabase.confluent.io",
-          "status.zoom.us",
-          "signin.aws.amazon.com",
-
-          /^go$/,
-          /confluent-internal\.io/,
-          /confluent\.io/,
-          /confluent\.cloud/,
-          /cultureamp\.com/,
-          /golinks\.io/,
-          /semaphoreci\.com/,
-          /sumologic\.com/,
-        ]),
-
-        /applications\.zoom\.us\/slack\/api\/call\/callback/,
-        /confluent\.zoom\.us\/rec\/play/,
-        /confluent\.zoom\.us\/saml/,
-        /github\.com\/.*confluentinc/,
-        /github\.com\/semaphoreci/,
-        /jetbrains\.com/,
-        /travis-ci\.org\/github\/confluentinc/,
-       ],
-    {%@@ if confluent @@%}
-      browser: "Google Chrome",
-    {%@@ elif personal @@%}
-      url: ({ urlString }) => {
-        return openInFirefoxContainer("CFLT", urlString);
-      },
-      browser: "Firefox",
-    {%@@ endif @@%}
-    },
-    {
       match: [
         finicky.matchHostnames([
           "my.asu.edu",
@@ -173,21 +122,21 @@ module.exports = {
 
           /^go$/,
           /confluent-internal\.io/,
-          /confluent.io/,
+          /confluent\.io/,
           /confluent\.cloud/,
-          /cultureamp.com/,
-          /geekbot\.com/,
-          /golinks.io/,
-          /semaphoreci.com/,
+          /cultureamp\.com/,
+          /golinks\.io/,
+          /semaphoreci\.com/,
+          /sumologic\.com/,
         ]),
 
-        /applications.zoom.us\/slack\/api\/call\/callback/,
-        /confluent.zoom.us\/rec\/play/,
-        /confluent.zoom.us\/saml/,
-        /github.com\/{{@@ github_account @@}}(\/|$)/,
-        /github.com\/.*confluentinc(\/|$)/,
-        /github.com\/semaphoreci(\/|$)/,
-        /jetbrains.com/,
+        /applications\.zoom\.us\/slack\/api\/call\/callback/,
+        /confluent\.zoom\.us\/rec\/play/,
+        /confluent\.zoom\.us\/saml/,
+        /github\.com\/.*confluentinc/,
+        /github\.com\/semaphoreci/,
+        /jetbrains\.com/,
+        /travis-ci\.org\/github\/confluentinc/,
        ],
     {%@@ if confluent @@%}
       browser: "Google Chrome",
