bf7a4d51a8bcce31562814e4ac341838a103401f
Adam Brown <acourtneybrown@gmail.com>
2024-04-22 15:21:59 -0700

    Add `colormap` alias to print terminal colors

--- dotfiles/oh-my-zsh-custom/plugins/aliases/aliases.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/aliases/aliases.plugin.zsh
@@ -73,3 +73,5 @@ alias zcp='zmv -C'
 alias zln='zmv -L'
 
 alias flush-dns='sudo dscacheutil -flushcache; sudo killall -HUP mDNSResponder'
+
+alias colormap='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%6)):#3}:+$'\\\\n'}; done'
