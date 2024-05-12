32dd8d0bd9dde0091f731281e16485e68f1d3ea1
Adam Brown <acourtneybrown@users.noreply.github.com>
2024-05-12 14:56:57 -0700

    Remove brave from Windows install (#224)

--- script/win-admin-setup.ps1
+++ script/win-admin-setup.ps1
@@ -7,7 +7,6 @@ Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://cho
 # TODO: follow https://github.com/chocolatey-community/chocolatey-packages/issues/1773
 # choco install 1password
 
-choco install -y brave
 choco install -y carnac
 choco install -y epicgameslauncher
 choco install -y firefox
