6d03e4cd478e0a3bf345786c3708db84398f0404
Adam Brown <acourtneybrown@gmail.com>
2024-05-18 21:13:29 -0700

    Pull GitHub cli (`gh`) functions/aliases out of `git-custom` plugin into `gh` one

---
+++ dotfiles/oh-my-zsh-custom/plugins/gh/gh.plugins.zsh
@@ -0,0 +1,67 @@
+# shellcheck disable=SC2148
+
+# shellcheck disable=SC2154
+if [[ ${commands[gh]} ]]; then
+  # _ghclall is an internal function that clones all of the non-archived, repos for a given user/org
+  function _ghclall() {
+    if [[ "${#}" -ne 1 ]]; then
+      return 1
+    fi
+
+    gh api "${1}" --paginate --jq '.[] | select(.archived == false) | .full_name' | xargs -n 1 -I % -P 6 -t gh repo clone %
+  }
+
+  # ghclorg clones all of the non-archived repos under a GitHub org
+  function ghclorg() {
+    if [[ "${#}" -ne 1 ]]; then
+      echo "Usage: ghclorg <org>"
+      return
+    fi
+    _ghclall "orgs/${1}/repos"
+  }
+
+  # ghcluser clones all of the non-archived repos under a GitHub user
+  function ghcluser() {
+    if [[ "${#}" -ne 1 ]]; then
+      echo "Usage: ghcluser <org>"
+      return
+    fi
+
+    if [[ ${1} == $(git config github.user) ]]; then
+      _ghclall "user/repos?type=owner"
+    else
+      _ghclall "users/${1}/repos"
+    fi
+  }
+
+  # ghrmarchived deletes the local clone for any repo that has been archived on GitHub
+  function ghrmarchived() {
+    local archived
+    for DIR in *; do
+      if [[ -d "${DIR}/.git" ]]; then
+        archived=$(
+          cd "$DIR" || return
+          gh api "repos/{owner}/{repo}" --jq '.archived'
+        )
+        $archived && {
+          echo "removing archived repo '$DIR'"
+          rm -rf -- "$DIR"
+        }
+      fi
+    done
+  }
+
+  # ghrmdeleted deletes the local clone for any repo that has been deleted on GitHub
+  function ghrmdeleted() {
+    for DIR in *; do
+      if [[ -d "${DIR}/.git" ]]; then
+        if ! (cd "$DIR" && gh api --silent "repos/{owner}/{repo}" 2>/dev/null); then
+          echo "removing deleted repo '$DIR'"
+          rm -rf -- "$DIR"
+        fi
+      fi
+    done
+  }
+
+  alias ghrmremoved='ghrmdeleted; ghrmarchived'
+fi
