3b5a906b49a0b1f60d5f02585b5e56ebfb50820b
Adam Brown <acourtneybrown@users.noreply.github.com>
2024-06-12 10:07:28 -0700

    Update mosh config (#250)
    * Add Homebrew paths to `.zshenv`
    
    * Remove `~/.ssh/id_ed25519` file tracking
    
    * Update dotdrop template to check if `ssh_agent` defined & non-empty
    
    * Fix condition
    
    * Make ssh-agent lifetime configurable via dotdrop variables
    
    * Remove ForwardAgent setting from `~/.ssh/config`
    
    * Switch to minimal, custom ssh-agent plugin
    
    * Fix bad reformatting by SublimeText
    
    * Make ssh-agent-custom plugin use an anonymous function
    
    * Remove ssh-agent-custom plugin attempt
    
    * Add function to load SSH key from 1Password into ssh-agent
    
    * Add "mosh beelink" profile
    
    * Remove blank line
    
    * Ensure logged into 1Password account for `op-ssh-add`

--- dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
@@ -1,8 +1,18 @@
 # shellcheck disable=SC2148
 
-# shellcheck disable=SC2157
-if [[ -n "{{@@ ssh_agent @@}}" ]]; then
-  export SSH_AUTH_SOCK="{{@@ ssh_agent @@}}"
-fi
-
+{%@@ if ssh_agent @@%}
+export SSH_AUTH_SOCK="{{@@ ssh_agent @@}}"
+{%@@ else @@%}
 alias ops='eval $(op signin)'
+
+# op-ssh-add loads an SSH private key from 1Password into a running ssh-agent.
+# By default, it loads the `host_ssh_key_name` key from the dotdrop config.
+function op-ssh-add() {
+  if ! op whoami >/dev/null; then
+    ops
+  fi
+
+  local key="${1:-{{@@ host_ssh_key_name @@}}}"
+  op read "op://Private/${key}/private_key?ssh-format=openssh" | ssh-add -
+}
+{%@@ endif @@%}
