70cf269b7bc2ef9ba818ebde9bf1362657ff0f7d
Adam Brown <acourtneybrown@gmail.com>
2024-06-11 19:52:58 -0700

    Add function to load SSH key from 1Password into ssh-agent

--- dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
@@ -1,8 +1,14 @@
 # shellcheck disable=SC2148
 
-# shellcheck disable=SC2157
-if [[ -n "{{@@ ssh_agent @@}}" ]]; then
-  export SSH_AUTH_SOCK="{{@@ ssh_agent @@}}"
-fi
-
+{%@@ if ssh_agent @@%}
+export SSH_AUTH_SOCK="{{@@ ssh_agent @@}}"
+{%@@ else @@%}
 alias ops='eval $(op signin)'
+
+# op-ssh-add loads an SSH private key from 1Password into a running ssh-agent.
+# By default, it loads the `host_ssh_key_name` key from the dotdrop config.
+function op-ssh-add() {
+  local key="${1:-{{@@ host_ssh_key_name @@}}}"
+  op read "op://Private/${key}/private_key?ssh-format=openssh" | ssh-add -
+}
+{%@@ endif @@%}
