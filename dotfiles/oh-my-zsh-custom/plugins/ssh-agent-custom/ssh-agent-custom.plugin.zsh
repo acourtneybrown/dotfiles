d4e5cf9716a665612dec24e0f51d164a403536de
Adam Brown <acourtneybrown@gmail.com>
2024-06-11 17:27:07 -0700

    Switch to minimal, custom ssh-agent plugin

---
+++ dotfiles/oh-my-zsh-custom/plugins/ssh-agent-custom/ssh-agent-custom.plugin.zsh
@@ -0,0 +1,33 @@
+# Get the filename to store/lookup the environment from
+ssh_env_cache="$HOME/.ssh/environment-$SHORT_HOST"
+
+function _start_agent() {
+  # Check if ssh-agent is already running
+  if [[ -f "$ssh_env_cache" ]]; then
+    . "$ssh_env_cache" >/dev/null
+
+    # Test if $SSH_AUTH_SOCK is visible
+    zmodload zsh/net/socket
+    if [[ -S "$SSH_AUTH_SOCK" ]] && zsocket "$SSH_AUTH_SOCK" 2>/dev/null; then
+      return 0
+    fi
+  fi
+
+  if [[ ! -d "$HOME/.ssh" ]]; then
+    echo "[oh-my-zsh] ssh-agent plugin requires ~/.ssh directory"
+    return 1
+  fi
+
+  # Set a maximum lifetime for identities added to ssh-agent
+  local lifetime
+  zstyle -s :omz:plugins:ssh-agent lifetime lifetime
+
+  # start ssh-agent and setup environment
+  zstyle -t :omz:plugins:ssh-agent quiet || echo >&2 "Starting ssh-agent ..."
+  ssh-agent -s ${lifetime:+-t} ${lifetime} | sed '/^echo/d' "$ssh_env_cache" >!
+  chmod 600 "$ssh_env_cache"
+  . "$ssh_env_cache" >/dev/null
+}
+
+unset ssh_env_cache
+unfunction _start_agent
