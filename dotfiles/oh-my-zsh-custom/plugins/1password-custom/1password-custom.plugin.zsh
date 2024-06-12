a4de02fdbb526f2a89d5f9d80b5fe230dd5ff381
Adam Brown <acourtneybrown@gmail.com>
2024-06-12 09:33:46 -0700

    Ensure logged into 1Password account for `op-ssh-add`

--- dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
@@ -8,6 +8,10 @@ alias ops='eval $(op signin)'
 # op-ssh-add loads an SSH private key from 1Password into a running ssh-agent.
 # By default, it loads the `host_ssh_key_name` key from the dotdrop config.
 function op-ssh-add() {
+  if ! op whoami >/dev/null; then
+    ops
+  fi
+
   local key="${1:-{{@@ host_ssh_key_name @@}}}"
   op read "op://Private/${key}/private_key?ssh-format=openssh" | ssh-add -
 }
