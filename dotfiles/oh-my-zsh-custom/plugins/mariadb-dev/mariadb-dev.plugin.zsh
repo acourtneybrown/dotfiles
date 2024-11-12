bd5c9643d0abfe1127163bdf667d52b5a01fa9c7
Adam Brown <acourtneybrown@gmail.com>
2024-11-12 10:43:04 -0700

    Only include `LD_LIBRARY_PATH` handling when `linux` set

--- dotfiles/oh-my-zsh-custom/plugins/mariadb-dev/mariadb-dev.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/mariadb-dev/mariadb-dev.plugin.zsh
@@ -2,7 +2,9 @@
 
 if [ -d "$(brew --prefix)/opt/mariadb-connector-c" ]; then
   path+="$(brew --prefix)/opt/mariadb-connector-c/bin"
+  {%@@ if linux @@%}
   # shellcheck disable=SC2155
   export LD_LIBRARY_PATH="$(brew --prefix)/opt/mariadb-connector-c/lib":${LD_LIBRARY_PATH}
   typeset -U LD_LIBRARY_PATH
+  {%@@ endif @@%}
 fi
