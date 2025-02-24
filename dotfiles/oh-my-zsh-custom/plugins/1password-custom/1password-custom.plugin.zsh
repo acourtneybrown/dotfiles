ecb40be1c0e3189e2ddb3ec72ad1c072ec25b11d
Adam Brown <acourtneybrown@gmail.com>
2025-02-24 15:23:35 -0700

    Make `sudo` request the password from 1Password

--- dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/1password-custom/1password-custom.plugin.zsh
@@ -4,6 +4,8 @@
 export SSH_AUTH_SOCK="{{@@ ssh_agent @@}}"
 {%@@ else @@%}
 alias ops='eval $(op signin)'
+alias sudo='sudo -A'
+export SUDO_ASKPASS=~/bin/sudopwd
 
 # op-ssh-add loads an SSH private key from 1Password into a running ssh-agent.
 # By default, it loads the `host_ssh_key_name` key from the dotdrop config.
