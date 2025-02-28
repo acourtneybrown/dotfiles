24fba453dbc09d1d1dd657256fa0f950795695ad
Adam Brown <acourtneybrown@gmail.com>
2025-02-28 16:12:40 -0700

    Switch `op plugin` aliases to functions to "fix" shell completions

--- dotfiles/config/op/plugins.sh
+++ dotfiles/config/op/plugins.sh
@@ -1,5 +1,13 @@
 export OP_PLUGIN_ALIASES_SOURCED=1
-alias twilio="op plugin run -- twilio"
-alias gh="op plugin run -- gh"
-alias glab="op plugin run -- glab"
-alias tea="op plugin run -- tea"
+function twilio() {
+	op plugin run -- twilio "$@"
+}
+function gh() {
+	op plugin run -- gh "$@"
+}
+function glab() {
+	op plugin run -- glab "$@"
+}
+function tea() {
+	op plugin run -- tea "$@"
+}
