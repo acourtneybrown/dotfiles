20bb876ac171dc2a70f7bd8ec47306428b9629db
Adam Brown <acourtneybrown@users.noreply.github.com>
2025-05-30 11:38:38 -0700

    Configure `eza` plugin(s) with oh-my-zsh (#320)
    * Configure `eza` plugin(s) with oh-my-zsh

---
+++ dotfiles/oh-my-zsh-custom/plugins/eza-custom/eza-custom.plugin.zsh
@@ -0,0 +1,7 @@
+# shellcheck disable=SC2148
+# Override `ls` aliases from oh-my-zsh/lib/directories.zsh to eza equivalent
+
+alias lsa='ls -laa'
+alias l='ls -laa'
+# ll alias already present in eza oh-my-zsh plugin
+# la alias already present in eza oh-my-zsh plugin
