9094b0f2d6778d689d0f6ee7e7939e77a2654e2f
Adam Brown <acourtneybrown@gmail.com>
2025-06-12 15:38:29 -0700

    Slight cleanup to dotall argument handling

--- dotfiles/oh-my-zsh-custom/plugins/graphviz/graphviz.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/graphviz/graphviz.plugin.zsh
@@ -5,11 +5,7 @@ if [[ ${commands[dot]} ]]; then
   # dotall will run the dot command over all .gv files in a directory & generate image.
   # by default, it will output png images, but an optional argument can specify a different type
   function dotall() {
-    local type
-    type=png
-    if [[ ${#} -eq 1 ]]; then
-      type="${1}"
-    fi
+    local type=${1:-png}
 
     find . -maxdepth 1 -name '*.gv' | while read -r file; do
       dot -T"${type}" "${file}" >"${file}.${type}"
