9e184a766e4f76016915d1be88764a9ab43fabb9
Adam Brown <acourtneybrown@gmail.com>
2025-06-22 18:56:44 -0700

    Fix typo in comment

--- dotfiles/oh-my-zsh-custom/plugins/alfred-workflow-dev/alfred-workflow-dev.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/alfred-workflow-dev/alfred-workflow-dev.plugin.zsh
@@ -76,7 +76,7 @@ function alfred-get-changes() {
 	local_workflow=$(_alfred-installed-workflow "$git_root")
 
     rsync --archive --delete --exclude prefs.plist "$local_workflow/" "$git_root/Workflow"
-    # TODO: ignore fields/files taht Alfred workflow packaging would
+    # TODO: ignore fields/files that Alfred workflow packaging would
 }
 
 # alfred-install-changes installs the local directory changes to the workflow
@@ -89,7 +89,7 @@ function alfred-install-changes() {
 	local_workflow=$(_alfred-installed-workflow "$git_root")
 
     rsync --archive --delete --exclude prefs.plist "$git_root/Workflow/" "$local_workflow"
-    # TODO: ignore fields/files taht Alfred workflow packaging would
+    # TODO: ignore fields/files that Alfred workflow packaging would
 }
 
 # alfred-diff-changes compares the contents of the local workflow directory
@@ -101,7 +101,7 @@ function alfred-diff-changes() {
 	local_workflow=$(_alfred-installed-workflow "$git_root")
 
     diff -urN --exclude prefs.plist "$local_workflow/" "$git_root/Workflow"
-    # TODO: ignore fields/files taht Alfred workflow packaging would
+    # TODO: ignore fields/files that Alfred workflow packaging would
 }
 
 # alfred-cd-installed changes the current directory to the installed
