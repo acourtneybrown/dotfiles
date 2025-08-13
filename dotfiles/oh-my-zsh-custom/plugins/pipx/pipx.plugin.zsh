8c6d05c30ae7b02c3d68f52b0c28a02649276314
Adam Brown <acourtneybrown@gmail.com>
2025-08-13 16:13:50 -0700

    Add `pipx_install` function to `pipx` plugin

--- dotfiles/oh-my-zsh-custom/plugins/pipx/pipx.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/pipx/pipx.plugin.zsh
@@ -10,4 +10,24 @@ if [[ ${commands[pipx]} ]]; then
     path=("${HOME}/.local/bin" ${path})
   fi
   _evalcache register-python-argcomplete pipx
+
+  # pipx_install takes a version of Python installed via pyenv & installs a list of pipx packages with that version
+  function pipx_install() {
+    local version
+    version=${1}
+    shift
+
+    local py_bin
+    py_bin=$(PYENV_VERSION=${version} pyenv prefix)/bin/python
+    local installed
+    installed=$(pipx list --json | jq '.venvs')
+
+    for package in "${@}"; do
+      if [[ $(jq "has(\"${package}\")" <<<"${installed}") == "false" ]]; then
+        pipx install "${package}" --python "${py_bin}"
+      else
+        pipx reinstall "${package}" --python "${py_bin}"
+      fi
+    done
+  }
 fi
