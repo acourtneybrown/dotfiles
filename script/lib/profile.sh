87ce23f9e16886c5c23270d1826e07dc7e7a1092
Adam Brown <acourtneybrown@gmail.com>
2026-05-22 15:19:06 -0700

    Check if `docker` command exists before trying to install `docker-ce`

--- script/lib/profile.sh
+++ script/lib/profile.sh
@@ -77,7 +77,7 @@ function profile::linux_dev() {
     libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
     libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
-  profile::install_docker_ce
+  command -v docker >/dev/null || profile::install_docker_ce
   sudo adduser "$(whoami)" docker
 }
 
