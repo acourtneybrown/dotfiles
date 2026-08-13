348411a5160e906ef0e517dbade0978f238f4fff
Adam Brown <acourtneybrown@gmail.com>
2026-08-13 14:34:46 -0700

    Rename `ip` alias to `globalip` to avoid conflict with Linux tool

--- dotfiles/oh-my-zsh-custom/plugins/aliases/aliases.plugin.zsh
+++ dotfiles/oh-my-zsh-custom/plugins/aliases/aliases.plugin.zsh
@@ -9,7 +9,7 @@ alias rgrep="grep -r"
 alias week='date +%V'
 
 # IP addresses
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias globalip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en0"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
 
